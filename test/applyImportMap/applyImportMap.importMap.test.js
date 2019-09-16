756fd027e773a53fcc57dfd760ea3dcc99ce8b17
Damien Maillard <damien.maillard@dailymotion.com>
2019-09-16 16:04:05 +0200

    allow to target specific files thanks to exports in package.json

---
+++ test/applyImportMap/applyImportMap.importMap.test.js
@@ -0,0 +1,14 @@
+import { assert } from "@dmail/assert"
+import { applyImportMap } from "../../index.js"
+
+const actual = applyImportMap({
+  importMap: {
+    imports: {
+      "/.jsenv/importMap.json": "file:///Users/me/importMap.json",
+    },
+  },
+  href: "http://example.com/.jsenv/importMap.json",
+  importerHref: "http://example.com/folder/file.js",
+})
+const expected = "file:///User/me/folder/foo.js"
+assert({ actual, expected })
