ddb20a9f2309853146e18c8194809649054c5375
Damien Maillard <damien.maillard@dailymotion.com>
2019-10-22 17:37:53 +0200

    update tests

--- test/resolveUrl/resolveUrl.bare.test.js
+++ test/resolveUrl/resolveUrl.bare.test.js
@@ -2,18 +2,6 @@ import { assert } from "@dmail/assert"
 import { resolveUrl } from "../../index.js"
 
 {
-  const actual = resolveUrl("./file.js", "file:///Users/folder")
-  const expected = "file:///Users/file.js"
-  assert({ actual, expected })
-}
-
-{
-  const actual = resolveUrl("./file.js", "file:///Users/folder/")
-  const expected = "file:///Users/folder/file.js"
-  assert({ actual, expected })
-}
-
-{
   const actual = resolveUrl("foo", "http://domain.com")
   const expected = "http://domain.com/foo"
   assert({ actual, expected })
@@ -30,5 +18,3 @@ import { resolveUrl } from "../../index.js"
   const expected = "http://domain.com/file.js/foo"
   assert({ actual, expected })
 }
-
-{ const actual = resolveUrl('../file.js', 'http://domain.com')}
--- test/resolveUrl/resolveUrl.relative-inside.test.js
+++ test/resolveUrl/resolveUrl.relative-inside.test.js
@@ -2,6 +2,18 @@ import { assert } from "@dmail/assert"
 import { resolveUrl } from "../../index.js"
 
 {
+  const actual = resolveUrl("./file.js", "file:///Users/folder")
+  const expected = "file:///Users/file.js"
+  assert({ actual, expected })
+}
+
+{
+  const actual = resolveUrl("./file.js", "file:///Users/folder/")
+  const expected = "file:///Users/folder/file.js"
+  assert({ actual, expected })
+}
+
+{
   const actual = resolveUrl("./file.js", "http://domain.com")
   const expected = "http://domain.com/file.js"
   assert({ actual, expected })
