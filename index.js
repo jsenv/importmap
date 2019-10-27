a6434c097392201ce1b717c2163b8cd4a337e89f
Damien Maillard <damien.maillard@dailymotion.com>
2019-10-27 18:41:35 +0100

    remove jsenv dedicated helpers (keep it simple)

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 export { applyImportMap } from "./src/applyImportMap/applyImportMap.js"
 export { composeTwoImportMaps } from "./src/composeTwoImportMaps/composeTwoImportMaps.js"
 export { normalizeImportMap } from "./src/normalizeImportMap/normalizeImportMap.js"
-export {
-  normalizeImportMapForProject,
-} from "./src/normalizeImportMapForProject/normalizeImportMapForProject.js"
 export { resolveImport } from "./src/resolveImport/resolveImport.js"
-export { resolveImportForProject } from "./src/resolveImportForProject/resolveImportForProject.js"
 export { resolveSpecifier } from "./src/resolveSpecifier/resolveSpecifier.js"
-export {
-  resolveSpecifierForProject,
-} from "./src/resolveSpecifierForProject/resolveSpecifierForProject.js"
 export { resolveUrl } from "./src/resolveUrl/resolveUrl.js"
 export { sortImportMap } from "./src/sortImportMap/sortImportMap.js"
 export { wrapImportMap } from "./src/wrapImportMap/wrapImportMap.js"
--- src/normalizeImportMapForProject/normalizeImportMapForProject.js
+++
@@ -1,8 +0,0 @@
-import { normalizeImportMap } from "../normalizeImportMap/normalizeImportMap.js"
-
-export const normalizeImportMapForProject = (
-  importMap,
-  httpResolutionOrigin = "http://fake_origin_unlikely_to_collide.ext",
-) => {
-  return normalizeImportMap(importMap, httpResolutionOrigin)
-}
