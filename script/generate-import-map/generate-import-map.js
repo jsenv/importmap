a2d32fa949d2f1df50ffcc505c80a0dcc886c7e1
Damien Maillard <damien.maillard@dailymotion.com>
2020-09-30 13:31:59 +0200

    update dev files

--- script/generate-import-map/generate-import-map.js
+++ script/generate-import-map/generate-import-map.js
@@ -1,8 +1,17 @@
-import { generateImportMapForProjectPackage } from "@jsenv/node-module-import-map"
-import * as jsenvConfig from "../../jsenv.config.js"
+import {
+  getImportMapFromNodeModules,
+  generateImportMapForProject,
+} from "@jsenv/node-module-import-map"
+import { projectDirectoryUrl } from "../../jsenv.config.js"
 
-generateImportMapForProjectPackage({
-  ...jsenvConfig,
-  importMapFile: true,
-  jsConfigFile: true,
-})
+generateImportMapForProject(
+  [
+    getImportMapFromNodeModules({
+      projectDirectoryUrl,
+    }),
+  ],
+  {
+    projectDirectoryUrl,
+    jsConfigFile: true,
+  },
+)
--- script/test/test.js
+++ script/test/test.js
@@ -25,5 +25,4 @@ executeTestPlan({
       },
     },
   },
-  completedExecutionLogMerging: true,
 })
