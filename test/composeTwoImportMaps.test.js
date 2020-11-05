b581b089e9a41d6528bdc7e4860453726a69a910
Damien Maillard <damien.maillard@dailymotion.com>
2020-11-05 18:52:20 +0100

    import map composition should apply right importmap remapping to left importmap remappings

--- test/composeTwoImportMaps.test.js
+++ test/composeTwoImportMaps.test.js
@@ -1,50 +1,135 @@
 import { assert } from "@jsenv/assert"
 import { composeTwoImportMaps, sortImportMap } from "../index.js"
 
-const actual = sortImportMap(
-  composeTwoImportMaps(
-    {
-      imports: {
-        foo: "a",
-        a: "a",
-      },
-      scopes: {
-        foo: {
+{
+  const actual = sortImportMap(
+    composeTwoImportMaps(
+      {
+        imports: {
           foo: "a",
           a: "a",
         },
-        a: {},
       },
-    },
-    {
-      imports: {
-        foo: "b",
-        b: "b",
-      },
-      scopes: {
-        foo: {
+      {
+        imports: {
           foo: "b",
           b: "b",
         },
-        b: {},
       },
-    },
-  ),
-)
-const expected = {
-  imports: {
-    foo: "b",
-    a: "a",
-    b: "b",
-  },
-  scopes: {
-    foo: {
+    ),
+  )
+  const expected = {
+    imports: {
       foo: "b",
       a: "a",
       b: "b",
     },
-    a: {},
-    b: {},
-  },
+  }
+  assert({ actual, expected })
 }
-assert({ actual, expected })
+
+{
+  const actual = sortImportMap(
+    composeTwoImportMaps(
+      {
+        imports: {
+          foo: "a",
+          a: "a",
+        },
+        scopes: {
+          foo: {
+            foo: "a",
+            a: "a",
+          },
+          a: {},
+        },
+      },
+      {
+        imports: {
+          foo: "b",
+          b: "b",
+        },
+        scopes: {
+          foo: {
+            foo: "b",
+            b: "b",
+          },
+          b: {},
+        },
+      },
+    ),
+  )
+  const expected = {
+    imports: {
+      foo: "b",
+      a: "a",
+      b: "b",
+    },
+    scopes: {
+      foo: {
+        foo: "b",
+        a: "a",
+        b: "b",
+      },
+      a: {},
+      b: {},
+    },
+  }
+  assert({ actual, expected })
+}
+
+// resolve first top level import with second top level import
+{
+  const actual = sortImportMap(
+    composeTwoImportMaps(
+      {
+        imports: {
+          foo: "a",
+        },
+      },
+      {
+        imports: {
+          a: "./b.js",
+        },
+      },
+    ),
+  )
+  const expected = {
+    imports: {
+      foo: "./b.js",
+      a: "./b.js",
+    },
+  }
+  assert({ actual, expected })
+}
+
+// resolve in scopes too
+{
+  const actual = sortImportMap(
+    composeTwoImportMaps(
+      {
+        scopes: {
+          directory: {
+            foo: "./a.js",
+          },
+        },
+      },
+      {
+        scopes: {
+          directory: {
+            "./a.js": "./b.js",
+          },
+        },
+      },
+    ),
+  )
+  const expected = {
+    scopes: {
+      directory: {
+        "./a.js": "./b.js",
+        "foo": "./b.js",
+      },
+    },
+  }
+  assert({ actual, expected })
+}
