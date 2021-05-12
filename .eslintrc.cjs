8ebe0685de1229303fab97cc9e9387ed62f274ba
Damien Maillard <damien.maillard@dailymotion.com>
2021-05-12 15:56:40 +0200

    update eslint config and update node version in workflow

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,56 @@
-const { createEslintConfig } = require("@jsenv/eslint-config")
+const {
+  composeEslintConfig,
+  eslintConfigBase,
+  eslintConfigForPrettier,
+  eslintConfigToPreferExplicitGlobals,
+  jsenvEslintRules,
+  jsenvEslintRulesForImport,
+} = require("@jsenv/eslint-config")
 
-const config = createEslintConfig({
-  projectDirectoryUrl: __dirname,
-  importResolutionMethod: "import-map",
-})
+const eslintConfig = composeEslintConfig(
+  eslintConfigBase,
+  {
+    rules: jsenvEslintRules,
+  },
+  {
+    env: {
+      node: true,
+    },
+  },
+  {
+    plugins: ["import"],
+    settings: {
+      "import/resolver": {
+        "@jsenv/importmap-eslint-resolver": {
+          projectDirectoryUrl: __dirname,
+          importMapFileRelativeUrl: "./import-map.importmap",
+          node: true,
+        },
+      },
+    },
+    rules: jsenvEslintRulesForImport,
+  },
+  // inside *.cjs files:
+  // 1. restore commonJS "globals"
+  // 2. use commonjs module resolution
+  {
+    overrides: [
+      {
+        files: ["**/*.cjs"],
+        env: {
+          node: true,
+          commonjs: true,
+        },
+        settings: {
+          "import/resolver": {
+            node: {},
+          },
+        },
+      },
+    ],
+  },
+  eslintConfigToPreferExplicitGlobals,
+  eslintConfigForPrettier,
+)
 
-module.exports = config
+module.exports = eslintConfig
